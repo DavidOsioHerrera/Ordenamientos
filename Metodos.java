1e8c166a9ccf602ea2774ddd1d7a4d8177dd8ae6
DavidOsioHerrera <davidosioherrera@gmail.com>
2016-09-07 21:08:30 -0500

    Add files via upload

--- Metodos.java
+++ Metodos.java
@@ -1,15 +1,18 @@
 
 package ordenamientos;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Davidssd
  */
-public class Bubblesort {
+public class Metodos {
     
     
-    public static void sort(int[] arr){
+    public static void bubbleSort(int[] arr){
         
+        int swaps = 0;
         int n = arr.length;
         int temp;
         int ciclo = 1;
@@ -18,19 +21,26 @@ public class Bubblesort {
             
             ciclo = 0;
             
-            for (int i = 1; i < n; i++){
+            for (int i = 1; i < n-1; i++){
                 
                 if (arr[i - 1] > arr[i]){
 
                     temp = arr[ i - 1];                   
                     arr[i - 1] = arr[i];
                     arr[i] = temp; 
- 
+                    swaps++;
                     ciclo = i;
-                }    
+                }  
+                System.out.println(Arrays.toString(arr));
+                
+                
             }
             
+            
             n--;
+            
+            
         }
+        System.out.println("Numero de swaps: " +swaps);
     }
 }
